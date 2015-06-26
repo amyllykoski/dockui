9a291b5f0ef5e24b1c40c5a35c00ba24d690a727
Antti Myllykoski <amyllykoski@yahoo.com>
2015-06-26 07:54:41 -0700

    connection via http-proxy (go) to docker registry (getimages) works

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,14 +8,17 @@
  * Controller of the dockuiApp
  */
 angular.module('dockuiApp')
-  .controller('AboutCtrl', function ($scope, ImageListService) {
+  .controller('AboutCtrl', function ($scope, $log, ImageListService) {
     $scope.images = {};
 
+    $log.debug("Getting images...");
     $scope.images = ImageListService.getImageList()
     .success(function(images){
+      $log.debug("Got images", images);
       $scope.images = images;
     })
     .error(function(error) {
+        $log.error("GetImages failed", error);
       $scope.status = 'Unable to get image list: ' + error.message;
     });
 
--- app/scripts/services/imagelistservice.js
+++ app/scripts/services/imagelistservice.js
@@ -8,12 +8,13 @@
  * Service in the dockuiApp.
  */
 angular.module('dockuiApp')
-  .factory('ImageListService', function ($http) {
-    var dockerUrl = 'http://10.25.191.196:2375/images/json';
-
-    var getImageList = function() {
+  .factory('ImageListService', function ($http, $log) {
+  //  var dockerUrl = 'http://10.25.191.196:2375/images/json';
+  var dockerUrl = 'http://localhost:8005';
+  var getImageList = function() {
+      $log.debug("Making AJAX request to", dockerUrl);
       return $http.get(dockerUrl);
-    };
+  };
 
     return {
         getImageList : getImageList
