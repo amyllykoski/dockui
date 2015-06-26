82bf83a15919541841f1382e743100c917bc0137
Antti Myllykoski <amyllykoski@yahoo.com>
2015-06-26 08:42:07 -0700

    generalized proxy access

--- app/scripts/services/imagelistservice.js
+++ app/scripts/services/imagelistservice.js
@@ -10,7 +10,9 @@
 angular.module('dockuiApp')
   .factory('ImageListService', function ($http, $log) {
   //  var dockerUrl = 'http://10.25.191.196:2375/images/json';
-  var dockerUrl = 'http://localhost:8005';
+  var PROXY_URL = 'http://localhost:8005/';
+  var dockerUrl = PROXY_URL + '_10.25.191.196:2375/images/json';
+
   var getImageList = function() {
       $log.debug("Making AJAX request to", dockerUrl);
       return $http.get(dockerUrl);
